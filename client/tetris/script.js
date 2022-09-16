5c85b322de9238a83896ff36ed7ebe7436076295
Dako-VN <danieldo2712@gmail.com>
2022-09-15 21:54:14 -0400

    test console.log

---
+++ client/tetris/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelector('.grid div'))
+    const width = 10
+
+    console.log(squares)
+
+    
+
+})
