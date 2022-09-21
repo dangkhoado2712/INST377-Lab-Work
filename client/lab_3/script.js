8fbb781c8d5c1f2d8fe853b36de16d4eee0a856f
Dako-VN <danieldo2712@gmail.com>
2022-09-20 20:56:36 -0400

    Update script.js

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -43,6 +43,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if(slidePosition == (totalSlides -1) ) {
+    slidePosition = 0;
+  } else {
+    slidePosition +=1;
+  }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -53,6 +58,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+  if (slidePosition = 0) {
+    slidePosition = totalSlides;
+  } else {
+    slidePosition -=1;
+  }
   updateSlidePosition();
 }
 
